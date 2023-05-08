ec644b9ebc6693fc9bb803c333443cbd89887918
Yuvraj Maharshi <yogeshnda2018@gmail.com.com>
2023-05-08 21:58:12 +0530

    Oauthand sorting

--- Backend/Authentication/index.ts
+++ Backend/Authentication/index.ts
@@ -1,11 +1,15 @@
 import express,{Request,Response} from "express";
 import connection from "./config/db";
 import Grouter from "./Oauth/googleOauth";
+import DoctorRouter from "./Oauth/DoctorOauth";
+import cors from "cors"
 const app=express()
+app.use(cors())
 app.get("/",(req:Request,res:Response):void=>{
     res.send("hello")
 })
 app.use("/",Grouter)
+app.use("/doctor",DoctorRouter)
 app.listen(4500,async ()=>{
     try{
         await connection
