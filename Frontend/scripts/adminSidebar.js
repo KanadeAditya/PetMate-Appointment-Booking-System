d2b45d528c7d01c8e5e7284ea85fea2b8e495fdf
Ayush Kr Shanu <112754570+Ayush-kr-shanu@users.noreply.github.com>
2023-05-08 09:00:06 +0530

    script for fetching data from apis is added only adding link is left for AdminDoctor page, AdminUser page and AdminPets page in sidebar also

--- Frontend/scripts/adminSidebar.js
+++ Frontend/scripts/adminSidebar.js
@@ -7,29 +7,37 @@ const heading = document.createElement('h2');
 heading.textContent = 'Admin Details';
 sidebar.appendChild(heading);
 
-// Create the admin avatar image
-const avatar = document.createElement('img');
-avatar.src = './Files/admin.png';
-avatar.alt = 'Admin Avatar';
-sidebar.appendChild(avatar);
+// Fetch admin data from backend
+fetch('/api/admin')
+  .then(response => response.json())
+  .then(data => {
+    // Create the admin avatar image
+    const avatar = document.createElement('img');
+    avatar.src = data.avatar;
+    avatar.alt = 'Admin Avatar';
+    sidebar.appendChild(avatar);
 
-// Create the admin name
-const name = document.createElement('h3');
-name.textContent = 'Name: John Doe';
-sidebar.appendChild(name);
+    // Create the admin name
+    const name = document.createElement('h3');
+    name.textContent = `Name: ${data.name}`;
+    sidebar.appendChild(name);
 
-// Create the admin email
-const email = document.createElement('p');
-email.textContent = 'Email: johndoe@example.com';
-sidebar.appendChild(email);
+    // Create the admin email
+    const email = document.createElement('p');
+    email.textContent = `Email: ${data.email}`;
+    sidebar.appendChild(email);
 
-// Create the logout button
-const logoutButton = document.createElement('button');
-logoutButton.textContent = 'Logout';
-logoutButton.addEventListener('click', () => {
-  // Handle logout logic here
-});
-sidebar.appendChild(logoutButton);
+    // Create the logout button
+    const logoutButton = document.createElement('button');
+    logoutButton.textContent = 'Logout';
+    logoutButton.addEventListener('click', () => {
+      // Handle logout logic here
+    });
+    sidebar.appendChild(logoutButton);
 
-// Append the sidebar to the document body
-document.body.appendChild(sidebar);
+    // Append the sidebar to the document body
+    document.body.appendChild(sidebar);
+  })
+  .catch(error => {
+    console.error('Error fetching admin data:', error);
+  });
