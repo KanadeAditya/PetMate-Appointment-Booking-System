e43dac3cd983b5bdccb14de0846c54c1971fea98
KanadeAditya <110182805+KanadeAditya@users.noreply.github.com>
2023-05-17 15:21:07 +0530

    changing frontend in react and ts

---
+++ Frontend/petmate/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PETMATE</h1>
+    </div>
+  );
+}
+
+export default App;
